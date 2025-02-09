8c5fa93b5491d5bd4ce59cb00444194516f58fb0
AmirHossein Hosseinian <amirhirx@gmail.com>
2025-02-09 21:50:16 +0330

    Add task to the list and save on localStorage

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 const $ = document
 const container = $.querySelector(".container")
 const newTaskPopup = $.getElementById("new-task-popup")
+const newTaskTitle = $.getElementById("new-task-title")
+const newTaskDescription = $.getElementById("new-task-description")
 const addTaskButton = $.getElementById("add-task-btn")
+const newTaskPopupAdd = $.getElementById("new-task-popup-add")
 const newTaskPopupCancel = $.getElementById("new-task-popup-cancel")
 const messagePopup = $.getElementById("message-popup")
 const messagePopupTitle = $.getElementById("message-popup-title")
@@ -22,20 +25,39 @@ messagePopupBtn.addEventListener("click", () => {
     container.classList.remove("blur")
 })
 
-message("سلام!", "این یک پیام آزمایشی است.")
+newTaskPopupAdd.addEventListener("click", () => {
+    let tasks = loadTasks()
+    let newTask = {
+        title: newTaskTitle.value,
+        description: newTaskDescription.value
+    }
+    newTaskTitle.value = ""
+    newTaskDescription.value = ""
+    tasks.push(newTask)
+    localStorage.setItem("tasks", JSON.stringify(tasks))
+    newTaskPopup.classList.remove("show")
+    container.classList.remove("blur")
+})
 
 function message(title, text) {
-    if(title){
+    if (title) {
         messagePopupTitle.innerText = title
-        if(text){
+        if (text) {
             messagePopupText.innerText = text
-        }else{
+        } else {
             messagePopupText.innerText = ""
         }
         messagePopup.classList.add("show")
         container.classList.add("blur")
-    
-    }else{
+
+    } else {
         console.log("Error!")
     }
+}
+
+
+function loadTasks() {
+    let tasks = localStorage.getItem("tasks")
+    tasks = tasks ? JSON.parse(tasks) : []
+    return tasks
 }
