6a126be7e009217ee01feff00191652f7e27fcae
AmirHossein Hosseinian <amirhirx@gmail.com>
2025-02-10 16:08:15 +0330

    Add renderTask for shw task in the task list

--- script.js
+++ script.js
@@ -10,6 +10,9 @@ const messagePopup = $.getElementById("message-popup")
 const messagePopupTitle = $.getElementById("message-popup-title")
 const messagePopupText = $.getElementById("message-popup-text")
 const messagePopupBtn = $.getElementById("message-popup-btn")
+const taskList = $.getElementById("task-list")
+
+$.addEventListener("DOMContentLoaded", renderTasks())
 
 addTaskButton.addEventListener("click", () => {
     newTaskPopup.classList.add("show")
@@ -37,6 +40,7 @@ newTaskPopupAdd.addEventListener("click", () => {
     localStorage.setItem("tasks", JSON.stringify(tasks))
     newTaskPopup.classList.remove("show")
     container.classList.remove("blur")
+    renderTasks()
 })
 
 function message(title, text) {
@@ -55,9 +59,29 @@ function message(title, text) {
     }
 }
 
-
 function loadTasks() {
     let tasks = localStorage.getItem("tasks")
     tasks = tasks ? JSON.parse(tasks) : []
     return tasks
+}
+
+function renderTasks() {
+    let tasks = loadTasks()
+    taskList.innerHTML = ""
+
+    tasks.map((task) => {
+        let taskElem = $.createElement("li")
+        taskElem.classList.add("task")
+
+        let taskTitleElem = $.createElement("h2")
+        taskTitleElem.innerText = task.title
+        taskElem.append(taskTitleElem)
+
+        if (task.description) {
+            let taskDescriptionElem = $.createElement("p")
+            taskDescriptionElem.innerText = task.description
+            taskElem.append(taskDescriptionElem)
+        }
+        taskList.append(taskElem)
+    })
 }
